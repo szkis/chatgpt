66f843047dcb72edd25ad57432420e593a680442
szkis <105256669+szkis@users.noreply.github.com>
2023-03-28 14:00:46 +0800

    Add files via upload

---
+++ src/system.ts
@@ -0,0 +1,27 @@
+import type { Model } from "./types"
+
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  password: "",
+  systemRule: "",
+  model: "gpt-3.5-turbo" as Model
+}
+
+export const defaultMessage = `Powered by OpenAI Vercel
+- 我们衷心感谢您的访问和支持，如果您有任何疑问或建议，请随时联系我们。我们将竭尽全力为您提供最好的服务!
+- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
+
+export type Setting = typeof defaultSetting
+
+export const defaultResetContinuousDialogue = false
+
+export const defaultMaxInputTokens: Record<Model, number> = {
+  "gpt-3.5-turbo": 3072,
+  "gpt-4": 6144,
+  "gpt-4-32k": 24576
+}
+
+export const defaultModel: Model = "gpt-3.5-turbo"
